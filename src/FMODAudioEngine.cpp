ca537586524fdb5e1ee29871014852d86e773084
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-04-18 16:37:50 -0400

    Revert "aktimoose again"
    This reverts commit 89087426a27dcee409f172e6302daa8fa6f49901.

--- src/FMODAudioEngine.cpp
+++ src/FMODAudioEngine.cpp
@@ -11,11 +11,7 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 	#ifndef GEODE_IS_MACOS
 	void playEffectAdvanced(gd::string p0, float p1, float p2, float p3, float p4, bool p5, bool p6, int p7, int p8, int p9, int p10, bool p11, int p12, bool p13, bool p14, int p15, int p16, float p17, int p18) {
 		auto manager = Manager::getSharedInstance();
-		auto badSFX = manager->badSFX;
 		auto vanillaSFX = manager->vanillaSFX;
-		if (std::find(badSFX.begin(), badSFX.end(), std::string(p0)) == badSFX.end() || Utils::isSceneRunning("PauseLayer") || !(Utils::modEnabled() && Utils::get("disableRewardsSFX")) || !manager->isPlayerDead) {
-			FMODAudioEngine::playEffectAdvanced(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18);
-		}
 		std::string path = p0;
 		std::smatch match;
 		if (path.find("geode") != std::string::npos && path.find("mods") != std::string::npos) {
@@ -27,8 +23,16 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 				manager->lastPlayedEffect = fmt::format("{}", path);
 			}
 		}
+		FMODAudioEngine::playEffectAdvanced(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18);
 	}
 	#endif
+	void playEffect(gd::string p0, float p1, float p2, float p3) {
+		auto manager = Manager::getSharedInstance();
+		auto badSFX = manager->badSFX;
+		if (std::find(badSFX.begin(), badSFX.end(), std::string(p0)) == badSFX.end() || Utils::isSceneRunning("PauseLayer") || !(Utils::modEnabled() && Utils::get("disableRewardsSFX")) || !manager->isPlayerDead) {
+			FMODAudioEngine::playEffect(p0, p1, p2, p3);
+		}
+	}
 	void loadMusic(gd::string p0, float p1, float p2, float p3, bool p4, int p5, int p6) {
 		auto manager = Manager::getSharedInstance();
 		std::string path = p0;
