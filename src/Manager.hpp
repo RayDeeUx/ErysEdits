f469bf809a7d188d350a48c575b8527b590d430a
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-04-22 17:05:35 -0400

    expunge c++ header file war crimes

--- src/Manager.hpp
+++ src/Manager.hpp
@@ -70,11 +70,10 @@ public:
 	
 	// GLenum glBlendFuncs[] = {GL_ZERO, GL_ONE, GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR, GL_DST_COLOR, GL_ONE_MINUS_DST_COLOR, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA, GL_SRC_ALPHA_SATURATE };
 
-	static Manager* getSharedInstance(){
-
+	static Manager* getSharedInstance() {
 		if (!instance) {
 			instance = new Manager();
-		};
+		}
 		return instance;
 	}
 
