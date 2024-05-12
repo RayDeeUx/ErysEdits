235e914ca5c9c8a6acc0775577c239997276778f
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-05-11 23:26:39 -0400

    comments + combo burst support

--- src/FMODAudioEngine.cpp
+++ src/FMODAudioEngine.cpp
@@ -11,23 +11,30 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 	#ifndef __APPLE__
 	void preloadEffect(gd::string p0) {
 		FMODAudioEngine::sharedEngine()->preloadEffect(p0);
+		if (!PlayLayer::get()) return;
 		auto manager = Manager::getSharedInstance();
 		auto vanillaSFX = manager->vanillaSFX;
 		std::string path = p0;
 		std::smatch match;
 		if (path.find("geode") != std::string::npos && (path.find("mods") != std::string::npos || path.find("config") != std::string::npos)) {
 			if (path.find("flafy.autonong") != std::string::npos || path.find(".autonong") != std::string::npos) {
-				manager->lastPlayedEffect = "[From Auto NONG]";
+				manager->lastPlayedEffect = "[From Auto NONG]"; // better safe than sorry
 			} else if (path.find("fleym.nongd") != std::string::npos || path.find(".nongd") != std::string::npos) {
-				manager->lastPlayedEffect = "[From Jukebox]";
+				manager->lastPlayedEffect = "[From Jukebox]"; // better safe than sorry
 			} else if (path.find("adam_729.randdeathsounds") != std::string::npos || path.find(".randdeathsounds") != std::string::npos) {
 				manager->lastPlayedEffect = "[From RandDeathSounds]";
+			} else if (path.find("crewly.comboburst") != std::string::npos || path.find(".comboburst") != std::string::npos) {
+				manager->lastPlayedEffect = "[From Combo Burst]"; // fallback in case Combo Burst does its thing
 			} else {
 				manager->lastPlayedEffect = "[From another Geode mod]";
 			}
 		} else if (std::find(vanillaSFX.begin(), vanillaSFX.end(), path) == vanillaSFX.end()) {
 			if (std::regex_match(path, match, songEffectRegex)) {
-				manager->lastPlayedEffect = fmt::format("{}.{}", match[1].str(), match[2].str());
+				if (path.find("cb_char") != std::string::npos) {
+					manager->lastPlayedEffect = "[From Combo Burst]"; // fallback for current combo burst behavior as of may 11 2024
+				} else {
+					manager->lastPlayedEffect = fmt::format("{}.{}", match[1].str(), match[2].str());
+				}
 			} else {
 				manager->lastPlayedEffect = fmt::format("{}", path);
 			}
@@ -42,6 +49,8 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 		}
 	}
 	void loadMusic(gd::string p0, float p1, float p2, float p3, bool p4, int p5, int p6) {
+		FMODAudioEngine::loadMusic(p0, p1, p2, p3, p4, p5, p6);
+		if (!PlayLayer::get()) return;
 		auto manager = Manager::getSharedInstance();
 		std::string path = p0;
 		std::smatch match;
@@ -58,6 +67,5 @@ class $modify(MyFMODAudioEngine, FMODAudioEngine) {
 		} else {
 			manager->lastPlayedSong = fmt::format("{}", path);
 		}
-		FMODAudioEngine::loadMusic(p0, p1, p2, p3, p4, p5, p6);
 	}
 };
