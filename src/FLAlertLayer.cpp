1b8ddad7d7a06e96321299a85d07d7ce366eb560
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-05-21 11:35:09 -0400

    prevter COOKED [untested]
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/FLAlertLayer.cpp
+++ src/FLAlertLayer.cpp
@@ -5,60 +5,40 @@
 using namespace geode::prelude;
 
 class $modify(MyFLAlertLayer, FLAlertLayer) {
-	struct Fields {
-		Manager* manager = Manager::getSharedInstance();
-	};
 	bool init(FLAlertLayerProtocol* delegate, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
-		if (Utils::modEnabled()) {
-			// apparently i can't use nullptr delegate here, so have a hacky workaround
-			std::string titleString = title;
-			if (titleString.find("T&^JKIU*HBJUDRFGCHU&^TRDFCGVBJHU*^%RDF") != std::string::npos) {
-				return FLAlertLayer::init(
-					delegate,
-					fmt::format(
-						"{} mods, {} disabled, {} problems",
-						m_fields->manager->installedMods,
-						m_fields->manager->disabledMods,
-						m_fields->manager->problems
-					).c_str(),
-					desc,
-					btn1,
-					btn2,
-					420.f,
-					true,
-					height,
-					1.f
-				);
-			} else if (titleString == "Always Active LDM (READ!)" || titleString.find("Show/Copy Mods List") != std::string::npos) {
-				return FLAlertLayer::init(
-					delegate,
-					title,
-					desc,
-					btn1,
-					btn2,
-					420.f,
-					false,
-					height,
-					.69f
-				);
-			} else {
-				bool desiredScroll = scroll;
-				float desiredScale = textScale;
-				float desiredWidth = width;
-				if (Utils::getInt("textScroll") < 1) {
-					if (!desiredScroll) { desiredScale = Utils::getDouble("textScale"); }
-					if (width <= 420.f) {
-						desiredWidth = width * Utils::getDouble("flAlertWidthMultiplier");
-						if (desiredWidth >= 420.f) { desiredWidth = 420.f; }
-					}
-				} else {
-					desiredWidth = 420.f;
-					desiredScroll = (Utils::getInt("textScroll") != -1);
-				}
-				return FLAlertLayer::init(delegate, title, desc, btn1, btn2, desiredWidth, desiredScroll, height, desiredScale);
-			}
-		} else {
+		if (!Utils::modEnabled()) {
 			return FLAlertLayer::init(delegate, title, desc, btn1, btn2, width, scroll, height, textScale);
 		}
+		// apparently i can't use nullptr delegate here, so have a hacky workaround
+		std::string titleString = title;
+		bool desiredScroll = scroll;
+		float desiredScale = textScale;
+		float desiredWidth = width;
+		if (titleString.find("T&^JKIU*HBJUDRFGCHU&^TRDFCGVBJHU*^%RDF") != std::string::npos) {
+			auto manager = Manager::getSharedInstance();
+			title = fmt::format(
+				"{} mods, {} disabled, {} problems",
+				manager->installedMods,
+				manager->disabledMods,
+				manager->problems
+			);
+		} else if (titleString == "Always Active LDM (READ!)" || titleString.find("Show/Copy Mods List") != std::string::npos) {
+			desiredScroll = true;
+			desiredScale = 1.f;
+			desiredWidth = 420.f;
+		} else {
+			if (Utils::getInt("textScroll") < 1) {
+				if (!desiredScroll) { desiredScale = static_cast<float>(Utils::getDouble("textScale")); }
+				if (width <= 420.f) {
+					desiredWidth = static_cast<float>(width * Utils::getDouble("flAlertWidthMultiplier"));
+					if (desiredWidth >= 420.f) { desiredWidth = 420.f; }
+				}
+			} else {
+				desiredWidth = 420.f;
+				desiredScroll = (Utils::getInt("textScroll") != -1);
+			}
+		}
+		
+		return FLAlertLayer::init(delegate, title, desc, btn1, btn2, desiredWidth, desiredScroll, height, desiredScale);
 	}
 };
