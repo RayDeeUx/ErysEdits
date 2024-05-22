502e9f8eb46ab1ae3778e3b286e802adc7cb5ba4
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-05-21 20:34:06 -0400

    ...how much did i break this time
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/MenuLayer.cpp
+++ src/MenuLayer.cpp
@@ -26,6 +26,28 @@ class $modify(MyMenuLayer, MenuLayer) {
 		auto problems = geode->getProblems().size();
 		
 		std::string modsString = "";
+		std::string modsStringForClipboard = "";
+		#ifdef __APPLE__
+		std::map<std::string, int> devNameToModCount{};
+		#else
+		switch (Utils::getInt("showModsListMode")) {
+			case 1:
+				m_fields->manager->modsListMode = "Mod names only";
+				break;
+			case 2:
+				m_fields->manager->modsListMode = "Mod names + version numbers only";
+				break;
+			case 3:
+				m_fields->manager->modsListMode = "Dev names + mod names only";
+				break;
+			case 4:
+				m_fields->manager->modsListMode = "Dev names, mod names, and version numbers";
+				break;
+			default:
+				m_fields->manager->modsListMode = "You've been naughty.";
+				break;
+		}
+		#endif
 		
 		std::ranges::for_each(mods, [&](const Mod *mod) {
 			std::string isEnabled = "<cg>";
@@ -38,24 +60,44 @@ class $modify(MyMenuLayer, MenuLayer) {
 				activeMods++;
 			}
 			
-			m_fields->manager->modsInfoForClipboard = m_fields->manager->modsInfoForClipboard + fmt::format("- {} ({}) {} by {} [{}]\n", isEnabledClipboard, mod->getID(), mod->getName(), mod->getDevelopers()[0], mod->getVersion().toString());
+			modsStringForClipboard = modsStringForClipboard + fmt::format("- {} ({}) {} by {} [{}]\n", isEnabledClipboard, mod->getID(), mod->getName(), mod->getDevelopers()[0], mod->getVersion().toString());
 
+			#ifndef __APPLE__
 			if (Utils::getInt("showModsListMode") == 4) { // mod name only
-				m_fields->manager->modsListMode = "Mod names only";
 				modsString = modsString + fmt::format("{}{}</c>, ", isEnabled, mod->getName());
 			} else if (Utils::getInt("showModsListMode") == 3) { // mod name and ver number only
-				m_fields->manager->modsListMode = "Mod names + version numbers only";
 				modsString = modsString + fmt::format("{}{} [{}]</c>, ", isEnabled, mod->getName(), mod->getVersion().toString());
 			} else if (Utils::getInt("showModsListMode") == 2) { // developer name and mod name only
-				m_fields->manager->modsListMode = "Dev names + mod names only";
 				modsString = modsString + fmt::format("{}{}'s {}</c>, ", isEnabled, mod->getDevelopers()[0], mod->getName());
 			} else if (Utils::getInt("showModsListMode") == 1) { // dev name, mod name, ver number
-				m_fields->manager->modsListMode = "Dev names, mod names, and version numbers";
 				modsString = modsString + fmt::format("{}{}'s {} [{}]</c>, ", isEnabled, mod->getDevelopers()[0], mod->getName(), mod->getVersion().toString());
+			} else { // fallback. because some people need to behave
+				modsString = modsString + fmt::format("- {} ({}) {} by {} [{}]\n", isEnabledClipboard, mod->getID(), mod->getName(), mod->getDevelopers()[0], mod->getVersion().toString());
 			}
+			#else
+			if (0 < Utils::getInt("showModsListMode") && Utils::getInt("showModsListMode") < 5) {
+				auto modDevName = mod->getDevelopers()[0];
+			   if (!devNameToModCount.contains(modDevName)) {
+				   devNameToModCount.insert(std::make_pair(modDevName, 1));
+			   } else {
+				   devNameToModCount.find(modDevName)->second += 1;
+			   }
+			}
+			#endif
 		
 		});
-		
+
+		#ifdef __APPLE__
+		m_fields->manager->modsListMode = "macOS Compat Mode";
+		if (0 < Utils::getInt("showModsListMode") && Utils::getInt("showModsListMode") < 5) {
+			for (auto const& [modDevName, numMods] : devNameToModCount) {
+				modsString.append(fmt::format("{}: {}, ", modDevName, numMods));
+			}
+		} else if (Utils::getInt("showModsListMode") != 0) {
+			modsString = "Installed mods list breakdown currently unavailable.\n<cr>Maybe don't break the settings file next time.</c>, ";
+		}
+		#endif
+
 		// remove ", " ending
 		if (!modsString.empty()) 
 		{
@@ -72,7 +114,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 		
 		m_fields->manager->modsInfo = modsString;
 		
-		m_fields->manager->modsInfoForClipboard = fmt::format("--- {} mods | {} loaded | {} disabled | {} problems ---\n", mods.size(), activeMods, disabledMods, problems) + m_fields->manager->modsInfoForClipboard + fmt::format("--- {} mods | {} loaded | {} disabled | {} problems ---", mods.size(), activeMods, disabledMods, problems);
+		m_fields->manager->modsInfoForClipboard = fmt::format("--- {} mods | {} loaded | {} disabled | {} problems ---\n", mods.size(), activeMods, disabledMods, problems) + modsStringForClipboard + fmt::format("--- {} mods | {} loaded | {} disabled | {} problems ---", mods.size(), activeMods, disabledMods, problems);
 		
 		// log::info("modsInfoForClipboard: {}", m_fields->manager->modsInfoForClipboard);
 		
