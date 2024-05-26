a8741bab9038d479795849435a047e4699122c3f
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-05-26 14:29:49 -0400

    prevter reported a bug, this is my attempt to fix it
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/PlayerObject.cpp
+++ src/PlayerObject.cpp
@@ -12,18 +12,55 @@ class $modify(MyPlayerObject, PlayerObject) {
 	{
 		(void) self.setHookPriority("PlayerObject::playerDestroyed", INT32_MAX - 1);
 	}
+	bool detectGlobed(PlayLayer* pl) {
+		if (Utils::getMod("dankmeme.globed2")) {
+			if (const auto pingLabel = pl->getChildByIDRecursive("dankmeme.globed2/ping-label")) {
+				if (const auto pingLabelBMFont = typeinfo_cast<CCLabelBMFont*>(pingLabel)) {
+					std::string pingLabelString = pingLabelBMFont->getString();
+					if (pingLabelString != "Not connected" && pingLabelString != "N/A (Local level)") {
+						return false;
+					}
+				}
+			}
+			if (const auto mainNode = pl->getChildByIDRecursive("main-node")) {
+				if (const auto batchLayer = mainNode->getChildByIDRecursive("batch-layer")) {
+					return MyPlayerObject::findGlobedChildren(batchLayer);
+				} else {
+					return MyPlayerObject::findGlobedChildren(mainNode);
+				}
+			}
+		}
+		return false;
+	}
+	bool findGlobedChildren(CCNode* node) {
+		auto children = CCArrayExt<CCNode*>(node->getChildren());
+		for (CCNode* child : children) {
+			std::string childID = child->getID();
+			if (childID != "") {
+				if (childID.rfind("dankmeme.globed2/remote-player-", 0) == 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	void playerDestroyed(bool p0) {
 		m_fields->manager->isPlayerDead = true;
 		if (Utils::modEnabled() && PlayLayer::get()) {
 			if (Utils::getBool("forceStopMusicOnDeath")) {
 				const auto pl = PlayLayer::get();
+				const bool isGlobed = MyPlayerObject::detectGlobed(pl);
 				const auto fmod = FMODAudioEngine::sharedEngine();
-				fmod->stopAllMusic();
+				if (!isGlobed) {
+					fmod->pauseAllMusic();
+				}
 				if (this == pl->m_player2 && pl->m_level->m_twoPlayerMode) {
 					PlayerObject::playerDestroyed(p0);
 					return; // avoid stopping sfx twice -- thank you clicksounds
 				} else {
-					fmod->stopAllEffects();
+					if (!isGlobed) {
+						fmod->stopAllEffects();
+					}
 				}
 			}
 		}
